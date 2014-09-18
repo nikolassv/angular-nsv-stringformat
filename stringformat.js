d7895ff1db75a751368b7c66f3526ca47a972d50
Nikolas Schmidt-Voigt <nikolas.schmidt-voigt@posteo.de>
2014-09-18 21:36:49 +0200

    added attributes for paragraphs, improved URL regEx

--- stringformat.js
+++ stringformat.js
@@ -21,53 +21,57 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
- angular.module('angular-nsv-stringformat',[])
-	.filter('paragraphs', function paragraphsFilterProvider () {
-		return function paragraphsFilter ($input) {
-			return angular.isString($input) ? '<p>'+ $input.replace(/\n\n+/gm , '</p><p>') + '</p>': '';
-		};
-	})
-	.filter('newlines', function newlinesFilterProvider () {
-		return function newlinesFilter ($input) {
-			return angular.isString($input) ? $input.replace(/\n/gm, '<br/>') : '';
-		};
-	})
-	.filter('stripTags', function stripTagsFilterProvider () {
-		return function stripTagsFilter ($input) {
-			return angular.isString($input) ? $input.replace(/<[^>]+/gm, '') : '';
-		};
-	})
-	.filter('sanitizeTags', function sanitizeTagsFilterProvider () {
-		return function sanitizeTagsFilter ($input) {
-			return angular.isString($input) ? $input.replace(/</gm, '&lt;').replace(/>/gm, '&gt;') : '';
-		}
-	})
-	.filter('linkUrls', function linkUrlsFilterProvider () {
-		var defaultProtocols = [
-			'http',
-			'https',
-			'ftp',
-			'file',
-		];
-		
-		return function linkUrlsFilter ($input, protocols, attrs) {
-			var attributesString = '';
-			if (angular.isObject(attrs)) {
-				for (var prop in attrs) {
-					if (attrs.hasOwnProperty(prop)) {
-						attributesString += ' '+prop+'="'+attrs[prop]+'"';
-					}
-				}
-			}
-			if (!angular.isArray(protocols)) {
-				protocols = defaultProtocols;
-			}
-			var urlRegexp = new RegExp('('+protocols.join('|')+')://\\S+', 'gi');
-			return angular.isString($input) ? $input.replace(urlRegexp, '<a href="$&"' + attributesString + '>$&</a>') : '';
-		};
-	})
-	.filter('trustAsHtml', ['$sce', function trustAsHtmlFilterProvider ($sce) {
-		return function trustAsHtmlFilter ($input) {
-			return $sce.trustAsHtml($input);
-		}
-	}]);
+(function () {
+  function createAttributesString (attrs) {
+    var attributesString = '';
+    if (angular.isObject(attrs)) {
+      for (var prop in attrs) {
+        if (attrs.hasOwnProperty(prop)) {
+          attributesString += ' '+prop+'="'+attrs[prop]+'"';
+        }
+      }
+    }
+    return attributesString;
+  }
+
+  angular.module('angular-nsv-stringformat',[])
+    .filter('paragraphs', function paragraphsFilterProvider () {
+      return function paragraphsFilter ($input, attrs) {
+        var pTag = '<p'+createAttributesString(attrs)+'>';
+        return angular.isString($input) ? 
+                pTag+ $input.replace(/\n\n+/gm , '</p>'+pTag)+'</p>'
+                : '';
+      };
+    })
+    .filter('newlines', function newlinesFilterProvider () {
+      return function newlinesFilter ($input) {
+        return angular.isString($input) ? $input.replace(/\n/gm, '<br/>') : '';
+      };
+    })
+    .filter('stripTags', function stripTagsFilterProvider () {
+      return function stripTagsFilter ($input) {
+        return angular.isString($input) ? $input.replace(/<[^>]+/gm, '') : '';
+      };
+    })
+    .filter('sanitizeTags', function sanitizeTagsFilterProvider () {
+      return function sanitizeTagsFilter ($input) {
+        return angular.isString($input) ? $input.replace(/</gm, '&lt;').replace(/>/gm, '&gt;') : '';
+      }
+    })
+    .filter('linkUrls', function linkUrlsFilterProvider () {
+      var urlRegexp = new RegExp(
+          '(https?|ftp|file)://' // the protocols
+          + '[A-Za-z0-9\\-_.~;:@=+$,/?%#[\\]!*\'()]+', // allowed chars in a url
+        'gi');
+      
+      return function linkUrlsFilter ($input, attrs) {
+        var attributesString = createAttributesString(attrs);
+        return angular.isString($input) ? $input.replace(urlRegexp, '<a href="$&"' + attributesString + '>$&</a>') : '';
+      };
+    })
+    .filter('trustAsHtml', ['$sce', function trustAsHtmlFilterProvider ($sce) {
+      return function trustAsHtmlFilter ($input) {
+        return $sce.trustAsHtml($input);
+      }
+    }]);
+}());
